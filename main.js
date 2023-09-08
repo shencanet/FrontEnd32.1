081ef0c97b94de89f1359ac1917ec10699e05082
shen <santi.dionis@gmail.com>
2023-09-08 21:35:04 +0200

    ADD: javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+const prevbtn = document.querySelector('#prev-btn');
+const nextbtn = document.querySelector('#next-btn');
+
+prevbtn.addEventListener('click', () => {
+    console.log('prev');
+});
+
+nextbtn.addEventListener('click', () => {
+    console.log('next');
+});
