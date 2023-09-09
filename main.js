92dab28b2181efa0b9f3671b351ca31221e3e463
shen <santi.dionis@gmail.com>
2023-09-09 11:25:02 +0200

    ADD: javascript

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-const prevbtn = document.querySelector('#prev-btn');
-const nextbtn = document.querySelector('#next-btn');
+const prevbtn = document.querySelector("#prev-btn");
+const nextbtn = document.querySelector("#next-btn");
+const userImg = document.querySelector("#user-img");
 
-prevbtn.addEventListener('click', () => {
-    console.log('prev');
+prevbtn.addEventListener("click", () => {
+    console.log(userImg);
+
+    userImg.setAttribute("src", "./images/image-john.jpg");
+
+
 });
 
-nextbtn.addEventListener('click', () => {
-    console.log('next');
-});
+nextbtn.addEventListener("click", () => {
+    console.log(userImg);
+
+    userImg.setAttribute("src", "./images/image-tanya.jpg");    
+ 
+  }
+
+);
