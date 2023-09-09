1118c9c13eef2146c8759710f20726458bfe8290
shen <santi.dionis@gmail.com>
2023-09-09 13:59:39 +0200

    ADD: javascript

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const prevbtn = document.querySelector("#prev-btn");
 const nextbtn = document.querySelector("#next-btn");
 const userImg = document.querySelector("#user-img");
-const user = [
+const users = [
   {
     name: "John Tarkpor",
     degree: "Junior Front-end Developer",
@@ -9,31 +9,35 @@ const user = [
     The depth the instructors go into is incredible. I now feel so confident about
     starting up as a professional developer. ”`,
     imageUrl: "./images/image-john.jpg",
-  },{
+  },
+  {
     name: "Tanya Sinclair",
     degree: "UX Engineer",
     testomonial: `“ I’ve been interested in coding for a while but never taken the jump, until now.
     I couldn’t recommend this course enough. I’m now in the job of my dreams and so
     excited about the future. ”`,
-    imgageUrl: "./images/image-tanya.jpg",
-
-  }
-  
-]
-
+    imageUrl: "./images/image-tanya.jpg",
+  },
+];
+ let imageIndex = 0; 
 prevbtn.addEventListener("click", () => {
-    console.log(userImg);
+  console.log(imageIndex);
+  imageIndex--
 
-    userImg.setAttribute("src", "./images/image-john.jpg");
-
-
+  userImg.setAttribute("src", users[imageIndex].imageUrl);
 });
 
 nextbtn.addEventListener("click", () => {
-    console.log(userImg);
+  if(imageIndex === users.length - 1){
+    imageIndex = 0;
+    console.log(imageIndex);
+  
 
-    userImg.setAttribute("src", "./images/image-tanya.jpg");    
- 
+
+  }else{
+    imageIndex++
   }
+  userImg.setAttribute("src", users[imageIndex].imageUrl);
 
-);
+
+});
